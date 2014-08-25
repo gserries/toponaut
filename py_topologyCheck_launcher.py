cefaea1d04b5cef7bca249dc486dded8fb54a92c
serries <gregory.serries@teledetection.fr>
2014-08-25 12:17:44 +0200

    Ajout de la fonction d'appel à la modification topologique

--- py_topologyCheck_launcher.py
+++ py_topologyCheck_launcher.py
@@ -7,7 +7,7 @@ from lib.py_topologyCheck_fonctions import topologyCheck
 
 ##Variables
 
-# Répertoire de l'environnement de travail"
+# Rï¿½pertoire de l'environnement de travail"
 workspace = "C:/Users/serries/Desktop/testControle_cours/"
 
 # CSV
@@ -18,38 +18,47 @@ csvObjet_file = csv_folder + "listObjet.csv"
 csvFeature_file = csv_folder + "listFeature.csv"
 csvRule_file = csv_folder + "listRule.csv"
 csvRuleBDTopage = csv_folder + "listRuleBDTopage.csv"
+csvGeometryFeature_file = csv_folder + "listGeometryFeature.csv"
 
-# Géodatabase topologie
+# Gï¿½odatabase topologie
 localisation = "0"
 geodatabase_topage="Controle_Topage_" + localisation + ".gdb"
 
 # Dataset source
 dataset_source = "source"
+
 # Dataset de la topo
 dataset_topology="topology"
+
+# Dataset de la gÃ©ometrie
+dataset_geometry="geometry"
 
 # Fichier pour la reference spatiale de la geometrie
 file_srid = "D:/ONEMA_GS/04_Produit/controleTopo/file/shape/TRONCON_COURS_EAU.SHP"
 
 # Nom de la topologie
 topology_name = "Topologie"
+geometry_name = "Geometrie"
 
 
 ### LANCEMENT DES SCRIPTS ###
 
-# Création de l'objet objets
+# Crï¿½ation de l'objet objets
 topologyTopageCheck = topologyCheck()
 
-# Création de la géodatabase de traitement
-topologyTopageCheck.createFramework(workspace,geodatabase_topage,dataset_source,dataset_topology,file_srid)
+# Crï¿½ation de la gï¿½odatabase de traitement
+#topologyTopageCheck.createFramework(workspace,geodatabase_topage,dataset_source,dataset_topology,dataset_geometry,file_srid)
 
-# Ajout des fichiers à traiter dans le dataset source
-topologyTopageCheck.add_sourceFeatureFromCSV(workspace + geodatabase_topage + "/" + dataset_source + "/",csvFeature_file,file_srid)
+# Ajout des fichiers ï¿½ traiter dans le dataset source
+#topologyTopageCheck.add_sourceFeatureFromCSV(workspace + geodatabase_topage + "/" + dataset_source + "/",csvFeature_file,file_srid)
 
-# Ajout des données à traiter dans le dataset topology
-topologyTopageCheck.add_topologyFeatureFromSourceDataset(topology_name,workspace + geodatabase_topage + "/" + dataset_source + "/",workspace + geodatabase_topage + "/" + dataset_topology + "/",csvFeature_file)
+# Ajout des donnï¿½es ï¿½ traiter dans le dataset topology
+#topologyTopageCheck.add_topologyFeatureFromSourceDataset(topology_name,workspace + geodatabase_topage + "/" + dataset_source + "/",workspace + geodatabase_topage + "/" + dataset_topology + "/",csvFeature_file)
 
-# Vérification de la topologie
-topologyTopageCheck.checkTopology(topology_name,workspace + geodatabase_topage + "/" + dataset_topology + "/",csvFeature_file,csvRule_file,csvRuleBDTopage)
+# Vï¿½rification de la topologie
+#topologyTopageCheck.checkTopology(topology_name,workspace + geodatabase_topage + "/" + dataset_topology + "/",csvFeature_file,csvRule_file,csvRuleBDTopage)
+
+# Vï¿½rification de la geometrie (continuitÃ© du rÃ©seau)
+topologyTopageCheck.checkGeometry(geometry_name,workspace + geodatabase_topage + "/" + dataset_topology + "/",workspace + geodatabase_topage + "/" + dataset_geometry + "/",csvGeometryFeature_file)
 
 #raw_input()
